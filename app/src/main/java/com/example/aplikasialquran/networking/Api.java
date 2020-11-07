4c353f51fce0c0eae7e3834d039763a871a908ed
Gilang <initial.gr.101@gmail.com>
2020-11-07 09:41:40 +0700

    Full kotlin

--- app/src/main/java/com/example/aplikasialquran/networking/Api.java
+++
@@ -1,9 +0,0 @@
-package com.example.aplikasialquran.networking;
-
-public class Api {
-
-    //API by BACHORS
-    public static String URL_LIST_SURAH = "https://api.npoint.io/99c279bb173a6e28359c/data";
-    public static String URL_LIST_AYAT = "https://api.npoint.io/99c279bb173a6e28359c/surat/{nomor}";
-
-}
